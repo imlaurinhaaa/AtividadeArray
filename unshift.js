ec3e9d64f8a62c6983a5c63b165fa3bfc8b3ac76
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2024-03-14 21:43:13 -0300

    método unshift

---
+++ unshift.js
@@ -0,0 +1,14 @@
+// Método .unshift()
+// ele é usado para adicionar um ou mais elementos no ínicio de um array.
+
+// primeiro exemplo: frutas;
+let frutas = ["maçã", "banana", "manga", "melancia", "goiaba"];
+frutas.unshift("abacaxi", "morango");
+console.log(frutas);
+// foi adicionado as frutas: acabaxi e morango no ínicio do array.
+
+// segundo exemplo: números;
+let numero = ["4", "5", "6"];
+numero.unshift("1", "2", "3");
+console.log(numero);
+// foi adicionado os números: 1, 2 e 3 no ínicio do array.
