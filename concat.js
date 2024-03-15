58852a6211ac4dbe9e7a5706836a39e0b34448d8
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2024-03-14 22:05:08 -0300

    método concat

---
+++ concat.js
@@ -0,0 +1,16 @@
+// Metódo .concat()
+// ele é usado para mesclar dois ou mais arrays, esse método não altera nenhum dos arrays, mas retorna um novo.
+
+// primeiro exemplo: alfabeto;
+let alfabeto1 = ["A", "B", "C"];
+let alfabeto2 = ["D", "E", "F"];
+let alfabeto3 = alfabeto1.concat(alfabeto2);
+console.log(alfabeto3);
+// neste exemplo ele juntou os arrays alfabeto1 com o alfabeto2 e criou um novo array, o alfabeto3.
+
+// segundo exemplo: vogais;
+let vogais1 = ["A","E"];
+let vogais2 = ["I", "O", "U"];
+let vogais3 = vogais1.concat(vogais2);
+console.log(vogais3);
+// neste exemplo ele juntou os arrays vogais1 com o vogais2 e criou um novo array, o vogais3.
