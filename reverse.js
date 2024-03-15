ae4234afd246fb88bd1dd27578127d9b5398b8a6
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2024-03-15 17:11:57 -0300

    método reverse

---
+++ reverse.js
@@ -0,0 +1,14 @@
+// Método .reserve()
+// ele é usado para everter nosso array, o que era primeiro se torna o último, e o que era último se torna o primeiro.
+
+// primeiro exemplo: países;
+let países = ["Brasil", "México", "China", "França", "Índia"];
+países.reverse();
+console.log(países);
+// ele inverteu o Brasil para último e a Ìndia para primeiro.
+
+// segundo exemplo: flores;
+let flores = ["margarida", "girassol", "rosa", "lírio", "tulipa"];
+flores.reverse();
+console.log(flores);
+// ele inverteu a margarida para último e a tulipa para primeiro.
