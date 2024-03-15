a0f0fcf8aeba40ce55adffaccd9b48779ab77815
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2024-03-14 22:30:23 -0300

    método join

---
+++ join.js
@@ -0,0 +1,12 @@
+// Método .join()
+// ele é usado para juntar todos os elementos de um array em uma string e retorna como uma string também.
+
+// primeiro exemplo: elementos;
+let elementos = ["água", "ar", "fogo", "terra"];
+console.log(elementos.join('-'));
+// ele separou os elementos.
+
+// segundo exemplo: cores do arco-íris;
+let arcoíris = ["vermelho", "laranja", "amarelo", "verde", "azul", "roxo"];
+console.log(arcoíris.join('-'));
+// ele separou as cores.
