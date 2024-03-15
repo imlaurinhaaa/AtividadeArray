364ed9f489208812748deee3da07c954909562ad
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2024-03-14 21:09:39 -0300

    método shift

---
+++ shift.js
@@ -0,0 +1,14 @@
+// Método .shift()
+// ele é usado para remover o primeiro elemento de um array. 
+
+// primeiro exemplo: cores;
+let cores = ["preto", "branco", "amarelo", "rosa", "verde"];
+cores.shift();
+console.log(cores);
+// neste exemplo o primeiro elemento removido do nosso array foi o "preto".
+
+// segundo exemplo: animais;
+let animais = ["peixe", "coelho", "leão", "tartaruga", "coruja"];
+animais.shift();
+console.log(animais);
+// neste exemplo o primeiro elemento removido do nosso array foi o "peixe".
