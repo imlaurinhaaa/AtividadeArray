32ce2397a23490b2d5c81220c52b59b6850d0abd
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2024-03-15 16:19:08 -0300

    método forEach

---
+++ foreach.js
@@ -0,0 +1,11 @@
+// Método .forEach()
+// ele é usado para executar uma dada função em cada elemento de um array.
+
+// primeiro exemplo: escola;
+let escola = ["lápis","caneta", "borracha" ];
+escola.forEach((element) => console.log(element));
+// neste exemplo ele executou cada elemnto que estava dentro do array.
+
+// segundo exemplo: matérias;
+let matérias = ["português", "matemática", "artes", "biologia", "geografia"];
+matérias.forEach((element) => console.log(element));
