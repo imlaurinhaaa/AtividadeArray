df072c6b4929c0d239b6d319c2b979d420e60e3b
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2024-03-15 17:53:17 -0300

    método slice

---
+++ slice.js
@@ -0,0 +1,15 @@
+// Método .slice()
+// ele é usado para retornar uma cópia de uma parte de um array a partir de um subarray criado entre as posições início e fim de um array original, o Array original não é modificado.
+
+// primeiro exemplo: estilo musicais ordem crescente;
+let música = ["pop", "funk", "rock", "sertanejo", "gospel"];
+console.log(música.slice(3));
+console.log(música.slice(1));
+// pop: 0/ funk: 1/ rock: 2/ sertanejo:3/ gospel:4.
+
+// segundo exemplo: estilo musicais ordem decrescente;
+let músicas = ["pop", "funk", "rock", "sertanejo", "gospel"];
+console.log(músicas.slice(0));
+console.log(músicas.slice(-4));
+console.log(músicas.slice(-1));
+// pop: 0/ funk: -4/ rock: -3/ sertanejo:-2/ gospel:-1.
